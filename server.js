5573ebb1cb68adf2630fbe8093ca6a04394d0ac2
lemonshushu <ifnt7luv@gmail.com>
2021-06-26 06:42:12 +0900

    update route

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/feeds/:name', (req, res) => {
+  res.sendFile(__dirname + `/feeds/${req.params.name}.xml`);
+});
+
 const server = app.listen(PORT, () => {
   console.log(`Server is listening`);
   setInterval(async () => {
